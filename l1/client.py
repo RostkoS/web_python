d9200020ee34c808aaf6624c680ac8e7ca278daa
Софія Росткович <sofiia.rostkovych.pz.2020@lpnu.ua>
2023-10-08 12:45:20 +0300

    l1_2

--- l1/client.py
+++ l1/client.py
@@ -1,10 +1,20 @@
-# echo-client.py
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+HEADER = 64
+HOST = "127.0.0.1"  
+PORT = 65432  
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
-mesg = f'{input("")}'
-client.send(mesg.encode('ascii'))
+mesg = f'{input("")}'.encode('ascii')
+mesg_len = len(mesg)
+send_len = str(mesg_len).encode('ascii')
+send_len += b' '*(HEADER - len(send_len))
+client.send(send_len)
+client.send(mesg)
+
+response_len = client.recv(HEADER).decode('ascii')
+response_len = int(response_len)
+response = client.recv(response_len).decode('ascii')
+         
+print(f"{response}")
--- l1/server.py
+++ l1/server.py
@@ -1,20 +1,27 @@
-# echo-server.py
-
+from ctypes import sizeof
 import socket
+import sys
 import time
 
+HEADER = 64
 HOST = "127.0.0.1"  
-PORT = 65432  
+PORT = 65432 
+t = time.localtime() 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
-
-
-       
+     
 while True:
             conn, addr = server.accept()
             print(f"Connected by {addr}")
-            mesg = conn.recv(1024).decode('ascii')
-            t = time.localtime()
+            mesg_len = conn.recv(HEADER).decode('ascii')
+            mesg_len = int(mesg_len)
+            mesg = conn.recv(mesg_len).decode('ascii')
             current_time = time.strftime("%H:%M:%S", t)
             print(f"{mesg} - {current_time}")
+            time.sleep(5)
+            
+            b = conn.send(f"{mesg_len}".encode('ascii'))
+            b = conn.send(f"{mesg}".encode('ascii'))
+            conn.close()
+            print("Connection closed")
