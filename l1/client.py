db43cd0ae9feab0687609b3e20aabc25b611144b
Софія Росткович <sofiia.rostkovych.pz.2020@lpnu.ua>
2023-10-08 13:43:10 +0300

    l1_3

--- l1/client.py
+++ l1/client.py
@@ -6,15 +6,18 @@ PORT = 65432
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
-mesg = f'{input("")}'.encode('ascii')
-mesg_len = len(mesg)
-send_len = str(mesg_len).encode('ascii')
-send_len += b' '*(HEADER - len(send_len))
-client.send(send_len)
-client.send(mesg)
 
-response_len = client.recv(HEADER).decode('ascii')
-response_len = int(response_len)
-response = client.recv(response_len).decode('ascii')
-         
-print(f"{response}")
+while True:
+    mesg = f'{input("")}'.encode('ascii')
+    mesg_len = len(mesg)
+    send_len = str(mesg_len).encode('ascii')
+    send_len += b' '*(HEADER - len(send_len))
+    client.send(send_len)
+    client.send(mesg)
+    response_len = client.recv(HEADER).decode('ascii')
+    if response_len != "":
+     response_len = int(response_len)
+     response = client.recv(response_len).decode('ascii')  
+     print(f"{response}")
+    else: 
+        break
--- l1/server.py
+++ l1/server.py
@@ -6,22 +6,31 @@ import time
 HEADER = 64
 HOST = "127.0.0.1"  
 PORT = 65432 
-t = time.localtime() 
+DISCONNECT_MESG = "DISCONNECT"
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
-server.listen()
-     
+server.listen()  
+
 while True:
-            conn, addr = server.accept()
-            print(f"Connected by {addr}")
-            mesg_len = conn.recv(HEADER).decode('ascii')
-            mesg_len = int(mesg_len)
-            mesg = conn.recv(mesg_len).decode('ascii')
+    print('Waiting for a connection...') 
+    conn, addr = server.accept()
+    print(f"Connected by {addr}")
+    while True:
+         mesg_len = conn.recv(HEADER).decode('ascii')
+         if mesg_len != "":
+           mesg_len = int(mesg_len)
+           mesg = conn.recv(mesg_len).decode('ascii')
+           if mesg == DISCONNECT_MESG:
+            print("Connection closed")
+            conn.close() 
+            break
+           else:
+            t = time.localtime() 
             current_time = time.strftime("%H:%M:%S", t)
             print(f"{mesg} - {current_time}")
             time.sleep(5)
-            
             b = conn.send(f"{mesg_len}".encode('ascii'))
             b = conn.send(f"{mesg}".encode('ascii'))
-            conn.close()
-            print("Connection closed")
+            
+            
