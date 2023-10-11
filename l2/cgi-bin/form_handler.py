448c0c6be6bc4c6903d50e5813e5a70ec545aa5a
Софія Росткович <sofiia.rostkovych.pz.2020@lpnu.ua>
2023-10-12 00:16:13 +0300

    l2.2

--- l2/cgi-bin/form_handler.py
+++ l2/cgi-bin/form_handler.py
@@ -17,9 +17,39 @@ for l in languages:
         value_choice = form.getvalue(l, "off")
         languages_checkbox[l] = value_choice
 if name=="" and surname=="":
-    name="uknown user" 
+    name="uknown" 
+    surname=="user"
 
-print("Content-type:text/html\r\n\r\n")
+print(f"Set-cookie: name={name};")
+print(f"Set-cookie: surname={surname};")
+print(f"Set-cookie: terms={terms};")
+print(f"Set-cookie: English={languages_checkbox["English"]};")
+print(f"Set-cookie: Ukrainian={languages_checkbox["Ukrainian"]};")
+print(f"Set-cookie: German={languages_checkbox["German"]};")
+cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+counter = 0
+
+def deleteCookies():
+    expired_date = "Wed, 28 Aug 2013 18:30:00 GMT"
+    print(f"Set-cookie: name={name}; expires={expired_date};")
+    print(f"Set-cookie: surname={name}; expires={expired_date};")
+    print(f"Set-cookie: terms={name}; expires={expired_date};")
+    print(f"Set-cookie: English={name}; expires={expired_date};")
+    print(f"Set-cookie: Ukrainian={name}; expires={expired_date};")
+    print(f"Set-cookie: German={name}; expires={expired_date};")
+    print(f"Set-cookie: counter={name}; expires={expired_date};")
+  
+name_cookie = cookie.get("name").value
+surname_cookie = cookie.get("surname").value
+terms_cookie = cookie.get("terms").value
+english_cookie = cookie.get("English").value
+ukrainian_cookie = cookie.get("Ukrainian").value
+german_cookie = cookie.get("German").value
+if cookie.get('counter').value >= 0:
+   counter = cookie['counter']
+   print(f"Set-cookie: counter={counter};")
+counter+=1
+print("Content-type:text/html\r\n")
 
 template_html = f"""
 <!DOCTYPE html>
@@ -33,6 +63,10 @@ template_html = f"""
     <h1>  </h1>
     <h2> {languages_checkbox=} </h2>
     <h2> {terms=} </h2>
+    <br>
+    <h2> From cookie: </h2>
+    <h3> {name_cookie=} {surname_cookie=} {terms_cookie=} {english_cookie=} {ukrainian_cookie=} {german_cookie=}</h3>
+    <h2>{counter=}</h2>
    </body>
 </html>
 """
