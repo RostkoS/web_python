d42597810a393ff53b098ac60b4d03a84949de26
Софія Росткович <sofiia.rostkovych.pz.2020@lpnu.ua>
2023-10-18 18:37:41 +0300

    l3.some_style

---
+++ l3/rostkovych_project/myenv/static/js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('hi').classList.toggle('.act');
+   setInterval(ch, 3000);
+});
+function ch()
+{
+
+    var t = document.getElementById('hi').classList;
+    if(t=="text .act"){
+document.getElementById('hi').classList.toggle('.act');
+document.getElementById('hi').style.opacity = 0;
+
+    document.getElementById('hello').classList.toggle('.act');
+    document.getElementById('hello').style.opacity=1;
+    //window.alert(t);
+    }
+    else{  
+        document.getElementById('hello').classList.toggle('.act');
+        document.getElementById('hello').style.opacity = 0;
+            document.getElementById('hi').classList.toggle('.act');
+            document.getElementById('hi').style.opacity=1;
+    }
+}
